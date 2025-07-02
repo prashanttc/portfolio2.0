f4c2cead699cc5dbb502200fe0d685d134e5633c
Prashant <prashantchouhan711@email.com>
2025-07-02 13:13:41 +0530

    ae pothos

--- data/projects.ts
+++ data/projects.ts
@@ -86,7 +86,7 @@ export const projects = [
       "A platform that allows whistleblowers to submit issues securely and anonymously.",
     fullDescription:
       "This secure platform allows users to report unethical behavior or concerns without revealing their identity. It uses encryption, anonymous session IDs, and metadata scrubbing to ensure complete privacy. Built with FastAPI and MongoDB, the system supports real-time updates, message replies, and internal case tracking. Ideal for NGOs, HR teams, or activist groups needing privacy-first issue reporting.",
-    images: ["/placeholder.svg?height=400&width=600"],
+    images: ["/ae1","/ae2.png","/ae23.png","/ae24.png","/ae25.png","/ae26.png"],
     technologies: [
       "React",
       "Python",
